46030e830616c2e3b152ec32f027ee4049647c21
samtru99 <samtrujillo1999@gmail.com>
2025-04-12 11:40:01 -0700

    Fixed up Base Case Conditions - Still missing outcomes

--- include/Generate_Q_Table.h
+++ include/Generate_Q_Table.h
@@ -15,9 +15,15 @@ class Gen_Q_Table
         int m_o_wins;
     
     public:
+        Gen_Q_Table()
+        {
+            m_draws = 0;
+            m_x_wins = 0;
+            m_o_wins = 0;
+        }
         void results();
         void init_function(std::vector< std::unordered_map< std::string,  std::vector < std::pair<std::string,double>>>>& Model_Data);
         void find_moves(std::vector< std::unordered_map< std::string,  std::vector < std::pair<std::string,double>>>>& Model_Data,
                         int level_num,  std::vector<char> x_current_move);
-        
+        void printBoard(std:: vector<char> board);
 };
--- src/Generate_Q_Table.cpp
+++ src/Generate_Q_Table.cpp
@@ -12,96 +12,109 @@
 
 void Gen_Q_Table::init_function(std::vector< std::unordered_map< std::string,  std::vector < std::pair<std::string,double>>>>& Model_Data)
 {
-    std::vector<char> board = {'_', '_', '_', '_', '_', '_', '_', '_', '_'};
-
-    for(int i = 0; i < 9; i++)
+    std::vector<char> board = {'_', '_', '_', '_', '_', '_', '_', '_', '_'};    
+    for(int x_first_move = 0; x_first_move < 9; x_first_move+=1)
     {
         std::vector<char> temp(board);
-        temp[i] = 'X';
+        temp[x_first_move] = 'X';
         std::string temp_str(temp.begin(), temp.end());
-        std::cout << "temp is now " << temp_str << std::endl;
+        std::cout << " X first move is  " << temp_str << std::endl;
         
         find_moves(Model_Data, 0, temp);
     }
-    std::cout << "GAME RESULTS " << std::endl;
-    std::cout << "X WINS " << m_x_wins << std::endl;
-    std::cout << "O WINS " << m_o_wins << std::endl;
-    std::cout << "DRAWS " << m_draws << std::endl;
-
 }
 
 void Gen_Q_Table::find_moves(std::vector< std::unordered_map< std::string,  std::vector < std::pair<std::string,double>>>>& Model_Data,
                         int level_num,  std::vector<char> x_current_move)
 {
     std::string x_current_move_str(x_current_move.begin(), x_current_move.end());
-    std::cout << "x_current_move is " << x_current_move_str << std::endl;
+    // std::cout << "x_current_move is " << x_current_move_str << std::endl;
 
     /*
         Bases Cases
         1. Full Board
         2. Winner on X moves 
     */
-    if( rows(x_current_move) || tie(x_current_move) || columns(x_current_move) || diagonals(x_current_move))
+    if( rows(x_current_move) || columns(x_current_move) || diagonals(x_current_move))
     {
-        if( rows(x_current_move))
-        {
-            m_x_wins += 1;
-            return;
-        }
-        else
-        {
-            m_draws += 1;
-            return;
-        }
+        std::cout << "X Won " << std::endl;
+        printBoard(x_current_move);
+        m_x_wins += 1;
+        return;
     }
-    else
+    else if ( tie(x_current_move))
     {
-        /*
-            1. 
-        */
+        std::cout << "Tie" << std::endl;
+        printBoard(x_current_move);
+        m_draws += 1;
+        return;
     }
-    // std::vector < std::pair<std::string,double>> x_moves_vector;
-    //Model_Data[level_num][str_board];// = std::vector < std::pair<std::string,double>>;
-    
+    else  
+    {
+        // std::vector < std::pair<std::string,double>> x_moves_vector;
+        //Model_Data[level_num][str_board];// = std::vector < std::pair<std::string,double>>;
 
-    // 1. Generate all O moves
-    for(int next_available_move_for_O = 0; next_available_move_for_O < 9; next_available_move_for_O++)
-    {
-        if(x_current_move[next_available_move_for_O] == '_')
+
+        // 1. Generate all O moves
+        for(int next_available_move_for_O = 0; next_available_move_for_O < 9; next_available_move_for_O++)
         {
-            // std::string O_next_move[9];
-            // std::copy(x_current_move, x_current_move + 9, O_next_move);
-            // O_next_move[next_available_move_for_O] = "O";
-            std::vector<char> O_next_move(x_current_move);
-            O_next_move[next_available_move_for_O] = 'O';
-            std::string O_next_move_str(O_next_move.begin(), O_next_move.end());
-            std::cout << "O_next_move - " << O_next_move_str << std::endl;
-            // Model_Data[level_num][str_board].push_back( std::make_pair( std::string(O_next_move), 0) );
-            
-            if( rows(O_next_move) || columns(O_next_move) || diagonals(O_next_move))
+            if(x_current_move[next_available_move_for_O] == '_')
             {
-                m_o_wins+=1;
-                return;
-            }
-            //Find X next move;
-            else
-            {
-                for(int find_x_counter_move = 0; find_x_counter_move < 9; find_x_counter_move+=1)
+                std::vector<char> O_next_move(x_current_move);
+                O_next_move[next_available_move_for_O] = 'O';
+                std::string O_next_move_str(O_next_move.begin(), O_next_move.end());
+                std::cout << "O_next_move - " << O_next_move_str << std::endl;
+                // Model_Data[level_num][str_board].push_back( std::make_pair( std::string(O_next_move), 0) );
+                
+                if( rows(O_next_move) || columns(O_next_move) || diagonals(O_next_move))
                 {
-                    if(O_next_move[find_x_counter_move] == '_')
+                    std::cout << "O won" << std::endl;
+                    printBoard(O_next_move);
+                    m_o_wins+=1;
+                    return;
+                }
+                //Find X next move;
+                else
+                {
+                    for(int find_x_counter_move = 0; find_x_counter_move < 9; find_x_counter_move+=1)
                     {
-                        // std::string x_counter_move[9];
-                        // std::copy(O_next_move, O_next_move + 9, x_counter_move);
-                        std::vector<char> x_counter_move(O_next_move);
-                        x_counter_move[find_x_counter_move] = 'X';
-                        std::string x_counter_move_str(x_counter_move.begin(), x_counter_move.end());
-                        std::cout << "x_counter_move is " << x_counter_move_str << std::endl;
+                        if(O_next_move[find_x_counter_move] == '_')
+                        {
+                            std::vector<char> x_counter_move(O_next_move);
+                            x_counter_move[find_x_counter_move] = 'X';
+                            std::string x_counter_move_str(x_counter_move.begin(), x_counter_move.end());
+                            std::cout << "x_counter_move is " << x_counter_move_str << std::endl;
 
-                        //find_moves(Model_Data, level_num+1, x_counter_move); //need to return the vector of pairs
+                            find_moves(Model_Data, level_num+1, x_counter_move); //need to return the vector of pairs
+                        }
                     }
                 }
             }
         }
     }
+}
 
+void Gen_Q_Table::results()
+{
+    /*
+        Goal
+        m_x_wins - 131,184
+        m_o_wins - 77,904
+        m_draws  - 46,080
+
+    */
+    std::cout << "GAME RESULTS " << std::endl;
+    std::cout << "TOTAL GAMES " << m_x_wins + m_o_wins + m_draws << std::endl;
+    std::cout << "X WINS " << m_x_wins << std::endl;
+    std::cout << "O WINS " << m_o_wins << std::endl;
+    std::cout << "DRAWS " << m_draws << std::endl;
+}
+
+void Gen_Q_Table::printBoard(std::vector<char> board)
+{
+    std::cout << "---------" << std::endl;
+    std::cout << "   " << board[0] << board[1] << board[2] << std::endl;
+    std::cout << "   " << board[3] << board[4] << board[5] << std::endl;
+    std::cout << "   " << board[6] << board[7] << board[8] << std::endl;
+    std::cout << "---------" << std::endl;
 }
