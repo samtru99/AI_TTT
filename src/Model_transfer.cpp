9d5eede5f8d0cdf9cdd0a79bbd1f881dd270880c
samtru99 <samtrujillo1999@gmail.com>
2025-05-10 17:20:59 -0700

    Fixed load_model function

--- src/Model_transfer.cpp
+++ src/Model_transfer.cpp
@@ -57,8 +57,12 @@ void Model_transfer::load_model(std::vector< std::map< std::string,  std::vector
 {
     //Iterate through each Q File
     std::cout << "Loading Model...." << std::endl;
-    for(int q_file_counter = 0; q_file_counter < 1; q_file_counter++)
+    for(int q_file_counter = 0; q_file_counter < 1 ; q_file_counter++) //m_Q_table_lists.size() 
     {
+        // Set up the Model
+        std::vector < std::pair<std::string,double>> temp_vector;
+
+
         std::string q_file = m_Q_table_lists[q_file_counter];
         std::cout << "Opening " << q_file << std::endl;
         std::ifstream model_file(q_file);
@@ -67,40 +71,50 @@ void Model_transfer::load_model(std::vector< std::map< std::string,  std::vector
         {
             //std::cout << Q_data << std::endl;
             std::string list_delimiter = ":";
-            std::string q_pair_delimiter = "||";
+            std::string q_pair_delimiter = "|";
             
             std::string key = Q_data.substr(0, Q_data.find(list_delimiter));
-            std::cout << "\n Key : " << key << std::endl;
+            temp_vector.clear();
+            //std::cout << "\n Key : " << key << std::endl;
 
             std::string values = Q_data.substr(10);
-            std::cout << "Values " << values << std::endl;
-            
             int tester = values.find(q_pair_delimiter);
-            std::cout << "tester is " << tester << std::endl;
             std::vector <std::pair<std::string, double>> list_q_pairs;
 
             int position_start = 0;
             int position_end = 0;
             std::string token;
-            std::cout << "Splitting " << std::endl;
+            //std::cout << "Splitting " << std::endl;
+            
+            int counter = 0;
+            counter = values.find(q_pair_delimiter);
+            while( (counter = values.find(q_pair_delimiter)) != std::string::npos)
+            {
 
-            // while((position_end = values.find("||", position_start) != std::string::npos ))
-            // {
-            //     token = values.substr(position_start, position_end - position_start);
-            //     std::cout << "Token is " << token << std::endl;;
-            //     position_start = position_end + q_pair_delimiter.size();
-            //     // res.push_back (token);
-            // }
-            // while((position = values.find("||") != std::string::npos))
-            // {
-            //     std::cout << "position is " << position << " ";
-            //     //std::cout << "-----" << std::endl;
-            //     token = values.substr(0, position);
-            //     std::cout << "Token is " << token << std::endl;;
-            //     values.erase(0,position);
-            // }
+                //Extract Q Pair
+                std::string new_pair = values.substr(0, counter);
+                //std::cout << "\tvalue = " << new_pair << std::endl;
+                values = values.substr(counter + 2);
+                //Store Q Pair
+                std::string o_move = new_pair.substr(0,9);
+                double q_value = stod(new_pair.substr(9));
+                temp_vector.push_back(std::make_pair(o_move,q_value));
 
+                //Check if extracting last value
+                if((counter = values.find(q_pair_delimiter)) == std::string::npos)
+                {
+                    new_pair = values;
+                    //std::cout << "\tvalue = " << new_pair << std::endl;
+                    //Store Q Pair
+                    std::string o_move = new_pair.substr(0,9);
+                    double q_value = stod(new_pair.substr(9));
+                    temp_vector.push_back(std::make_pair(o_move,q_value));
+                }
+
+            }
+            Model_Data[q_file_counter][key] = temp_vector;
         }
+
 
     }
 }
